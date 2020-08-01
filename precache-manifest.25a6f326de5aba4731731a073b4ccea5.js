efcdb75e753dbcdd6552dabc18558615c4f33b9d
arvindh123 <arvindh123@users.noreply.github.com>
2020-08-01 15:01:12 +0000

    deploy: 028edc0128a4b074cfecd96e8d964b6f425ff5bb

---
+++ precache-manifest.25a6f326de5aba4731731a073b4ccea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dcbe892a50fbe741b0a9366de9c54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d33a2dd7bb9bbc0257f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2fdbe39834a6cf86856",
+    "url": "/static/js/2.1e0ed463.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e0ed463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33a2dd7bb9bbc0257f2",
+    "url": "/static/js/main.be616852.chunk.js"
+  },
+  {
+    "revision": "71a00b5a0b515b33a875",
+    "url": "/static/js/runtime-main.030ec307.js"
+  }
+]);
